d911d4a31df913086ba6c2d25179bbc71871e847
Drowsy <59733218+Drowsy-Kat@users.noreply.github.com>
2024-02-12 20:48:28 +0000

    you can now view your current fav burrito also creating new views is becoming more and more tedius

--- app/src/main/java/com/example/barrysburritos/Cart.kt
+++ app/src/main/java/com/example/barrysburritos/Cart.kt
@@ -14,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -98,7 +99,7 @@ class Cart : Fragment() {
 
         view.findViewById<Button>(R.id.favoriteButton).setOnClickListener {
 
-            if(isFavoriteEmpty(requireContext())) {
+            if(!isFavoriteEmpty(requireContext())) {
                 Toast.makeText(requireContext(), "No favorites to add to cart", Toast.LENGTH_SHORT).show()
             }else {
                 val favoritesJson =
@@ -122,7 +123,84 @@ class Cart : Fragment() {
 
         view.findViewById<Button>(R.id.viewLastOrderButton).setOnClickListener {
             val allItems = readOrderFromJson(requireContext())
-            showPopup(allItems)
+            showLastOrderPopup(allItems)
+        }
+
+        view.findViewById<Button>(R.id.currentFavorite).setOnClickListener{
+            val favoritesJson =
+                premadeCartViewModel.returnFavoritesFromJsonAsString(requireContext())
+            val favorites = getFavorites(favoritesJson)
+            if (favorites is PremadeOrderItem) {
+                val premadeItem = premadeCartViewModel.convertToCartItem(favorites, premadeViewModel)
+                val dialog = Dialog(requireContext())
+                dialog.setContentView(R.layout.current_favorite_premade_popup)
+                val title = dialog.findViewById<TextView>(R.id.favouritePopupTitlePremade)
+                val description = dialog.findViewById<TextView>(R.id.favouritePopupDescription)
+                val price = dialog.findViewById<TextView>(R.id.favouritePopupPricePremade)
+                val quantity = dialog.findViewById<TextView>(R.id.favouritePopupQuantityCustom)
+                val image = dialog.findViewById<ImageView>(R.id.favouritePopupImage)
+                title.text = premadeItem.item.title
+                description.text = premadeItem.item.description
+                price.text = "£${"%.2f".format(premadeItem.item.price)}"
+                quantity.text = " ${premadeItem.quantity}"
+                val resourceId = getImageResourceByName(requireContext(), premadeItem.item.imageName)
+                image.setImageResource(resourceId)
+                dialog.findViewById<Button>(R.id.favouritePopupAddToCartPremade).setOnClickListener{
+                    premadeCartViewModel.addToCart(premadeItem)
+
+                    val allItems = mutableListOf<Any>()
+                    allItems.addAll(customCartViewModel.cartItems.value ?: emptyList())
+                    allItems.addAll(premadeCartViewModel.cartItems.value ?: emptyList())
+                    adapter.updateItems(allItems)
+                    dialog.dismiss()
+                }
+
+                dialog.show()
+
+
+            }
+            else if (favorites is CustomCartItem) {
+                val dialog = Dialog(requireContext())
+                dialog.setContentView(R.layout.current_favorite_custom_popup)
+
+                val name = dialog.findViewById<TextView>(R.id.burritoNameTextView)
+                val rice = dialog.findViewById<TextView>(R.id.riceTextView)
+                val beans = dialog.findViewById<TextView>(R.id.beansTextView)
+                val protein = dialog.findViewById<TextView>(R.id.proteinTextView)
+                val salsa = dialog.findViewById<TextView>(R.id.salsaTextViewFavorite)
+                val guac = dialog.findViewById<TextView>(R.id.guacamoleTextViewFavorite)
+                val cheese = dialog.findViewById<TextView>(R.id.cheeseTextViewFavorite)
+                val sourCream = dialog.findViewById<TextView>(R.id.sourCreamTextViewFavorite)
+                val quantity = dialog.findViewById<TextView>(R.id.favouritePopupQuantityCustom)
+                val price = dialog.findViewById<TextView>(R.id.favouritePopupPriceCustom)
+
+
+                name.text = favorites.burritoName
+                rice.text = favorites.riceChoice
+                beans.text = favorites.beansChoice
+                protein.text = favorites.proteinChoice
+                guac.visibility = if (favorites.guacamole) View.VISIBLE else View.GONE
+                salsa.visibility = if (favorites.salsa) View.VISIBLE else View.GONE
+                cheese.visibility = if (favorites.cheese) View.VISIBLE else View.GONE
+                sourCream.visibility = if (favorites.sourCream) View.VISIBLE else View.GONE
+
+                quantity.text = " ${favorites.quantity}"
+                price.text = "£${"%.2f".format(favorites.price * favorites.quantity)}"
+
+                dialog.findViewById<Button>(R.id.favouritePopupAddToCartCustom).setOnClickListener{
+                    customCartViewModel.addToCart(favorites)
+                    val allItems = mutableListOf<Any>()
+                    allItems.addAll(customCartViewModel.cartItems.value ?: emptyList())
+                    allItems.addAll(premadeCartViewModel.cartItems.value ?: emptyList())
+                    adapter.updateItems(allItems)
+                    dialog.dismiss()
+                }
+                dialog.show()
+
+            }else{
+                Toast.makeText(requireContext(), "No favorites to show", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
 
@@ -205,7 +283,7 @@ class Cart : Fragment() {
         return allItems
     }
 
-    fun showPopup(allItems: MutableList<Any>) {
+    fun showLastOrderPopup(allItems: MutableList<Any>) {
         val dialog = Dialog(requireContext())
         dialog.setContentView(R.layout.last_order_popup)
 
@@ -219,7 +297,15 @@ class Cart : Fragment() {
     }
 
 
-
+    @SuppressLint("DiscouragedApi")
+    private fun getImageResourceByName(context: Context, imageName: String): Int {
+        // Resolve image resource ID dynamically
+        return context.resources.getIdentifier(
+            imageName,
+            "drawable",
+            context.packageName
+        )
+    }
 
 
 
@@ -228,5 +314,5 @@ class Cart : Fragment() {
 
 
 
-// TODO: show details of the current favorite
+
 
